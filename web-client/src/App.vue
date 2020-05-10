0b685955bdd7ae21319b6652f76079629d48f5b9
karljacques <2953841+karljacques@users.noreply.github.com>
2020-05-10 16:08:42 +0100

    Simplify App.vue

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -1,17 +1,5 @@
 <template>
     <v-app id="inspire">
-        <v-navigation-drawer app clipped>
-            <v-list dense>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon>mdi-view-dashboard</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Dashboard</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
         <v-content>
             <v-container>
                 <router-view></router-view>
