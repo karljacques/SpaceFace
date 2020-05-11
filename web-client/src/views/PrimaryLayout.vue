c2cd8907a1721ab3390c71312032b9fc5f2c0cc8
karljacques <2953841+karljacques@users.noreply.github.com>
2020-05-11 18:48:43 +0100

    Rename Home.vue to PrimaryLayout.vue

--- web-client/src/views/PrimaryLayout.vue
+++ web-client/src/views/PrimaryLayout.vue
@@ -5,23 +5,13 @@
         <navigational-information v-if="shipLoaded"></navigational-information>
         <br>
         <general-status v-if="shipLoaded"></general-status>
-
-        <div v-if="entryNodes">
-            <div v-for="node in entryNodes">
-                target: ({{ node.exitLocation.system.designation }}) {{node.exitLocation.system.name}}
-
-                <button class="btn btn-primary" @click="jump(node.id)">Jump</button>
-            </div>
-        </div>
     </div>
 </template>
 
 <script lang="ts">
     import {Component} from 'vue-property-decorator';
 
-    import {WebSocketClient} from '@/services/connectivity/WebSocket';
     import NavigationalControls from '@/views/game/components/navigation/NavigationalControls.vue';
-    import {HttpInterface} from '@/services/connectivity/HttpInterface';
     import {VueContainer} from '@/VueContainer';
     import NavigationalInformation from '@/views/game/components/navigation/NavigationalInformation.vue';
     import {namespace} from 'vuex-class';
@@ -32,29 +22,9 @@
     @Component({
         components: {GeneralStatus, NavigationalInformation, NavigationalControls},
     })
-    export default class Home extends VueContainer {
-
+    export default class PrimaryLayout extends VueContainer {
         @ship.Getter
         protected shipLoaded!: boolean;
-
-        protected entryNodes: any[] = [];
-
-        protected http: HttpInterface = this.get(HttpInterface);
-        protected ws: WebSocketClient = this.get(WebSocketClient);
-
-        public async created() {
-            //
-            // this.ws.connect();
-            // this.refreshStatus();
-        }
-
-
-        protected async jump(node: number) {
-            const response = await this.http.post('/jump', {
-                node,
-            });
-        }
-
     }
 </script>
 
